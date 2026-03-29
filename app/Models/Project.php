08c6a56e3ded95235f9f190650503d306376cc9a
@muhammad-ari-kuncoro <@muhammad-ari-kuncoro>
2026-03-29 13:21:50 +0700

    add: softdelete function in models project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Project extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
 
     protected $table = 'menu_project';  // Sesuaikan dengan nama tabel di database
     // protected $guarded = ['id'];
